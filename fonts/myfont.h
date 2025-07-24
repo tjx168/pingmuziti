b41000bc61ed180cbbff6031450835480df048c6
tjx168 <1608492120@qq.com>
2025-07-24 15:29:26 +0800

    Update myfont.h

--- fonts/myfont.h
+++ fonts/myfont.h
@@ -13,19 +13,10 @@ typedef struct {
   uint16_t h;
 } ChineseChar;
 
-typedef struct {
-  const ChineseChar* char_ptr;
-  uint16_t unicode;
-} FontItem;
-
 extern const uint16_t myfont_bitmap[];
 extern const ChineseChar fontMap[];
 extern const size_t fontMapSize;
 extern const uint16_t supported_unicodes[];
 extern const size_t supported_unicodes_size;
 
-extern FontItem* available_chars;
-extern int char_count;
-
 const ChineseChar* findChar(uint16_t unicode);
-void detectAvailableChars();
